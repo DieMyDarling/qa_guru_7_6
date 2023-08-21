a6a5913ba912be2bb6d34675ed94dac131676a0b
Alexey <sjim2ez4u@gmail.com>
2023-08-21 17:27:33 +0800

    Homework for lesson 6

--- homework_6.py
+++ homework_6.py
@@ -83,12 +83,6 @@ def readable_function(func, *args):
     return result
 
 
-def test_readable_function():
-    open_browser(browser_name="Chrome")
-    go_to_companyname_homepage(page_url="https://companyname.com")
-    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
-
-
 def open_browser(browser_name):
     actual_result = readable_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
@@ -102,6 +96,12 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = readable_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
 test_readable_function()
